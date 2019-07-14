7ea6e137363a2bc89a90e74fd99ec381af568d09
xiakaifa <1131225133@qq.com>
2019-07-14 22:43:49 +0800

    增加了权限处理和部分UI代码

---
+++ AndroidClient/app/src/main/java/com/example/androidclient/AddressAdapter.kt
@@ -0,0 +1,16 @@
+package com.example.androidclient
+
+import android.view.View
+import androidx.recyclerview.widget.RecyclerView
+
+class AddressAdapter {
+
+}
+
+class ViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+
+
+    init {
+
+    }
+}
