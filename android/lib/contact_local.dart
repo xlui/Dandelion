20ce684218bb27f823275c082e47a8703d0ac577
xlui <liuqi0315@gmail.com>
2019-08-29 23:53:40 +0800

    UI for local contacts

--- android/lib/contact_local.dart
+++ android/lib/contact_local.dart
@@ -1,3 +1,4 @@
+import 'package:after_layout/after_layout.dart';
 import 'package:contacts_service/contacts_service.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
@@ -7,7 +8,16 @@ class LocalContact extends StatefulWidget {
   _LocalContactState createState() => _LocalContactState();
 }
 
-class _LocalContactState extends State<LocalContact> {
+class _LocalContactState extends State<LocalContact>
+    with AfterLayoutMixin<LocalContact> {
+  var _contacts = List<Contact>();
+
+  /// UI 加载完成后从本地加载联系人，并通过 setState 通知 UI 刷新
+  @override
+  void afterFirstLayout(BuildContext context) {
+    _loadContacts();
+  }
+
   @override
   Widget build(BuildContext context) {
     return new Scaffold(
@@ -20,22 +30,79 @@ class _LocalContactState extends State<LocalContact> {
     );
   }
 
+  /// 构建本地联系人界面
   Widget _drawBody() {
-    _loadContacts();
-    return Center(
-      child: Text(
-        'Local contacts',
-        style: TextStyle(fontSize: 20),
+    var listTiles = List<Widget>();
+    for (int i = 0; i < _contacts.length; i++) {
+      listTiles.add(_buildRow(_contacts[i]));
+      if (i != _contacts.length - 1) {
+        listTiles.add(Divider(
+          height: 1,
+          indent: 12,
+          endIndent: 12,
+        ));
+      }
+    }
+    return ListView(
+      children: listTiles,
+    );
+  }
+
+  /// 构建列表行
+  Widget _buildRow(Contact contact) {
+    return GestureDetector(
+      behavior: HitTestBehavior.translucent,
+      onTap: () {
+        showDialog(
+          context: context,
+          builder: (context) {
+            return AlertDialog(
+              title: Text(contact.displayName),
+              content: Text(
+                contact.phones.map((phone) => phone.value).join(', '),
+              ),
+            );
+          },
+        );
+        print('Tap list tile!');
+      },
+      child: Container(
+        padding: EdgeInsets.fromLTRB(16.0, 10.0, 16.0, 10.0),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: _buildContactInfo(contact),
+        ),
       ),
     );
   }
 
-  void _loadContacts() async {
-    var contacts = await ContactsService.getContacts();
-    contacts.forEach((contact) {
-      print(
-          '${contact.displayName}: ${contact.phones.map((item) =>
-          item.value)})}');
+  /// 构建联系人显示信息
+  List<Widget> _buildContactInfo(Contact contact) {
+    var contactInfo = List<Widget>();
+    contactInfo.add(Container(
+      padding: EdgeInsets.only(bottom: 8.0),
+      child: Text(
+        contact.displayName,
+        style: TextStyle(fontWeight: FontWeight.bold),
+      ),
+    ));
+    for (var item in contact.phones) {
+      contactInfo.add(
+        new Text(
+          item.value,
+          style: TextStyle(color: Colors.grey[500]),
+        ),
+      );
+    }
+    return contactInfo;
+  }
+
+  /// 加载联系人
+  void _loadContacts() {
+    ContactsService.getContacts().then((contacts) {
+      setState(() {
+        _contacts = contacts.toList(growable: false);
+      });
     });
   }
 }
